8bc1dd2ee740e4c4a0bb293ed53ebaeddc545d87
Ethan.Chiu <claratseng@Ethans-MacBook-Air.local>
2026-02-10 22:13:17 -0700

    Comments to question 6

--- Question 6.py
+++ Question 6.py
@@ -1,8 +1,9 @@
 import pandas as panda
 
+# Loads dataset
 df = panda.read_csv("crime.csv")
 
-
+# Function for High_crime and Low_crime
 def classify_risk(value):
     if value >= 0.50:
         return "High-Crime"
@@ -11,6 +12,7 @@ def classify_risk(value):
 
 df["risk"] = df["ViolentCrimesPerPop"].apply(classify_risk)
 
+# Calculates mean
 avg_unemployment = df.groupby("risk")["PctUnemployed"].mean()
 
 print("Average unemployment rate by risk group:")
